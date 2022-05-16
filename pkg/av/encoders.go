9253c203c160489c58f862b9288d8749d67274d5
Kevin Wang <kevmo314@gmail.com>
2022-05-15 23:29:39 -0400

    feat: add x264opts

--- pkg/av/encoders.go
+++ pkg/av/encoders.go
@@ -1,25 +1,25 @@
 package av
 
-var DefaultH264EncoderOptions = map[string]interface{} {
-	"preset": "veryfast",
-	"profile": "baseline",
+var DefaultH264EncoderOptions = map[string]interface{}{
+	"preset":   "veryfast",
+	"profile":  "baseline",
 	"x264opts": "bframes=0:force-cfr=1:mbtree=0:sync-lookahead=0:rc-lookahead=0",
 }
 
-var DefaultVP8EncoderOptions = map[string]interface{} {
+var DefaultVP8EncoderOptions = map[string]interface{}{
 	"deadline": "realtime",
 	"cpu-used": 5,
-	"crf": 20,
+	"crf":      20,
 }
 
-var DefaultVP9EncoderOptions = map[string]interface{} {
+var DefaultVP9EncoderOptions = map[string]interface{}{
 	"deadline": "realtime",
 	"cpu-used": 5,
-	"crf": 20,
+	"crf":      20,
 }
 
-var DefaultOpusEncoderOptions = map[string]interface{} {
-	"fec": 1,
+var DefaultOpusEncoderOptions = map[string]interface{}{
+	"fec":         1,
 	"packet_loss": 0,
-	"vbr": "constrained",
+	"vbr":         "constrained",
 }
